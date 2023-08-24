f9744d297dfa04c80b28df56a72eb83114487246
Soichiro Inatani <inai17ibar@gmail.com>
2023-08-25 08:54:37 +0900

    Assign object values to style name and text

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import TodoList  from './TodoList';
 
 export default function App() {
-  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
-  const description = 'Gregorio Y. Zara';
   return (
     <TodoList/>
   );
