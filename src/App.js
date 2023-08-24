c773d5e6d59c077d40fe7776d62278106b68395c
Soichiro Inatani <inai17ibar@gmail.com>
2023-08-25 08:50:44 +0900

    Embed style and cutout of TodoList.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
+import TodoList  from './TodoList';
 
 export default function App() {
   const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
   const description = 'Gregorio Y. Zara';
   return (
-    <img
-      className="avatar"
-      src={avatar}
-      alt={description}
-    />
+    <TodoList/>
   );
 }
