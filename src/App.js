988467022d73141dd8b54d554b04ed6e1cfd0430
Soichiro Inatani <inai17ibar@gmail.com>
2023-08-24 21:27:30 +0900

    Use a mix of default and named exports

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Gallery from './Gallery.js';
+import { Profile } from './Gallery.js';
 
 export default function App() {
   return (
-    <Gallery />
+    <Profile />
   );
 }
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import Profile from './Profile.js';
 
+export function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/QIrZWGIs.jpg"
+        alt="Alan L. Hart"
+      />
+    );
+  }
+  
 export default function Gallery() {
   return (
     <section>
