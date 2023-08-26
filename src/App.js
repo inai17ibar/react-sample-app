a063157f489053dda706216bae3fa1de3c68b790
Soichiro Inatani <inai17ibar@gmail.com>
2023-08-26 18:09:11 +0900

    How props change over time.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import './App.css';
-import Avatar from './Avatar.js';
+import { useState, useEffect } from 'react';
+import Clock from './Clock';
 
-function Card({ children }) {
-  return (
-    <div className="card">
-      {children}
-    </div>
-  );
+function useTime() {
+  const [time, setTime] = useState(() => new Date());
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return time;
 }
 
 export default function App() {
+  const time = useTime();
+  const [color, setColor] = useState('lightcoral');
   return (
-    <Card>
-      <Avatar
-        size={100}
-        person={{ 
-          name: 'Katsuko Saruhashi',
-          imageId: 'YfeOqp2'
-        }}
-      />
-    </Card>
+    <div>
+      <p>
+        Pick a color:{' '}
+        <select value={color} onChange={e => setColor(e.target.value)}>
+          <option value="lightcoral">lightcoral</option>
+          <option value="midnightblue">midnightblue</option>
+          <option value="rebeccapurple">rebeccapurple</option>
+        </select>
+      </p>
+      <Clock color={color} time={time.toLocaleTimeString()} />
+    </div>
   );
 }
---
+++ src/Clock.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Clock({ color, time }) {
+    return (
+      <h1 style={{ color: color }}>
+        {time}
+      </h1>
+    );
+  }
