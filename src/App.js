b71b2a6341770e0bb7ea541008a27c0d694e9479
Soichiro Inatani <inai17ibar@gmail.com>
2023-08-27 22:33:22 +0900

    Extract components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import React from 'react';
 import './App.css';
-import { useState, useEffect } from 'react';
-import Clock from './Clock';
-
-function useTime() {
-  const [time, setTime] = useState(() => new Date());
-  useEffect(() => {
-    const id = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-    return () => clearInterval(id);
-  }, []);
-  return time;
-}
+import Gallery from './Gallery';
 
 export default function App() {
-  const time = useTime();
-  const [color, setColor] = useState('lightcoral');
   return (
-    <div>
-      <p>
-        Pick a color:{' '}
-        <select value={color} onChange={e => setColor(e.target.value)}>
-          <option value="lightcoral">lightcoral</option>
-          <option value="midnightblue">midnightblue</option>
-          <option value="rebeccapurple">rebeccapurple</option>
-        </select>
-      </p>
-      <Clock color={color} time={time.toLocaleTimeString()} />
-    </div>
+    <Gallery></Gallery> 
   );
 }
--- src/Profile.js
+++ src/Profile.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import { getImageUrl } from './utils.js';
 
-//defaultをつけないとdefault exportをつかえない
-export default function Profile() {
+export default function Profile({
+    imageId,
+    name,
+    profession,
+    awards,
+    discovery,
+    imageSize = 70
+  }) {
     return (
-      <img
-        src="https://i.imgur.com/QIrZWGIs.jpg"
-        alt="Alan L. Hart"
-      />
+      <section className="profile">
+        <h2>{name}</h2>
+        <img
+          className="avatar"
+          src={getImageUrl(imageId)}
+          alt={name}
+          width={imageSize}
+          height={imageSize}
+        />
+        <ul>
+          <li><b>Profession:</b> {profession}</li>
+          <li>
+            <b>Awards: {awards.length} </b>
+            ({awards.join(', ')})
+          </li>
+          <li>
+            <b>Discovered: </b>
+            {discovery}
+          </li>
+        </ul>
+      </section>
     );
   }
+  
   
