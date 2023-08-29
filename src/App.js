cb429725b06fa523c9caa1ef985cc40f0d97f6f2
Soichiro Inatani <inai17ibar@gmail.com>
2023-08-29 23:03:31 +0900

    Conditionally returning JSX

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import './App.css';
-import Profile from './Profile';
+
+function Item({ name, isPacked }) {
+  if (isPacked) {
+    return <li className="item">{name} ✔</li>;
+  }
+  return <li className="item">{name}</li>;
+}
+
+function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item 
+          isPacked={true} 
+          name="Space suit" 
+        />
+        <Item 
+          isPacked={true} 
+          name="Helmet with a golden leaf" 
+        />
+        <Item 
+          isPacked={false} 
+          name="Photo of Tam" 
+        />
+      </ul>
+    </section>
+  );
+}
 
 export default function App() {
   return (
-    <Profile></Profile>
+    <PackingList></PackingList>
   );
 }
