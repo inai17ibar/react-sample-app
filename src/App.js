36330f0d3c7a7f7bde38b47d8483dee7481be6ef
Soichiro Inatani <inai17ibar@gmail.com>
2023-08-30 20:20:14 +0900

    Devide PackageList.js file.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,6 @@
 import React from 'react';
 import './App.css';
-
-function Item({ name, importance }) {
-  return (
-    <li className="item">
-      {name}
-      {importance > 0 && ' '}
-      {importance > 0 &&
-        <i>(importance: {importance})</i>
-      }
-    </li>
-  );
-}
-
-function PackingList() {
-  return (
-    <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-      <Item 
-          importance={9} 
-          name="Space suit" 
-        />
-        <Item 
-          importance={0} 
-          name="Helmet with a golden leaf" 
-        />
-        <Item 
-          importance={6} 
-          name="Photo of Tam" 
-        />
-      </ul>
-    </section>
-  );
-}
+import PackingList from './PackingList';
 
 export default function App() {
   return (
