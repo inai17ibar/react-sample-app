a0e7007759ebc142b2e8b2fad62d51cced0abc7a
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-02 00:00:31 +0900

    List with seperator

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import './App.css';
-import RecipeList from './RecipeList';
+
+const poem = {
+  lines: [
+    'I write, erase, rewrite',
+    'Erase again, and then',
+    'A poppy blooms.'
+  ]
+};
+
+function Poem() {
+  let output = [];
+
+  // Fill the output array
+  poem.lines.forEach((line, i) => {
+    output.push(
+      <hr key={i + '-separator'} />
+    );
+    output.push(
+      <p key={i + '-text'}>
+        {line}
+      </p>
+    );
+  });
+  // Remove the first <hr />
+  output.shift();
+
+  return (
+    <article>
+      {output}
+    </article>
+  );
+}
 
 export default function App() {
   return (
-    <RecipeList></RecipeList>
+    <Poem></Poem>
   );
 }
