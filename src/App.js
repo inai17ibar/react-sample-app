2574dd5c7b4a60c4032848dc818c66a66f7db66c
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-01 23:36:12 +0900

    Nested lists in one component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import './App.css';
-import List from './PeopleList';
+import { recipes } from './recipeData.js';
+
+function RecipeList() {
+  return (
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map(recipe =>
+        <div key={recipe.id}>
+        <h2>{recipe.name}</h2>
+        <ul>
+          {recipe.ingredients.map(ingredient =>
+            <li key={ingredient}>
+              {ingredient}
+            </li>
+          )}
+        </ul>
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default function App() {
   return (
-    <List></List>
+    <RecipeList></RecipeList>
   );
 }
