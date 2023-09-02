ea436d584b5ca376ad3480d6a40bec56f8745477
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-02 17:20:00 +0900

    Add poem.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Fragment } from 'react';
-
-const poem = {
-  lines: [
-    'I write, erase, rewrite',
-    'Erase again, and then',
-    'A poppy blooms.'
-  ]
-};
-
-function Poem() {
-  return (
-    <article>
-      {poem.lines.map((line, i) =>
-        <Fragment key={i}>
-          {i > 0 && <hr />}
-          <p>{line}</p>
-        </Fragment>
-      )}
-    </article>
-  );
-}
+import Poem from './poem';
 
 export default function App() {
   return (
