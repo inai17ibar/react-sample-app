25d8b3294c1107b7d436f10ee0f95b5e8d09fe8f
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-02 17:22:06 +0900

    Purity: Components as formulas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.css';
-import Poem from './poem';
+
+function Recipe({ drinkers }) {
+  return (
+    <ol>    
+      <li>Boil {drinkers} cups of water.</li>
+      <li>Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice.</li>
+      <li>Add {0.5 * drinkers} cups of milk to boil and sugar to taste.</li>
+    </ol>
+  );
+}
 
 export default function App() {
   return (
-    <Poem></Poem>
+    <section>
+      <h1>Spiced Chai Recipe</h1>
+      <h2>For two</h2>
+      <Recipe drinkers={2} />
+      <h2>For a gathering</h2>
+      <Recipe drinkers={4} />
+    </section>
   );
 }
