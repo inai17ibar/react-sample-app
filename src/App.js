8a1386f1c26400d4bcc4c5972520039816c22507
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-03 17:15:02 +0900

    Return JSX className field without using DOM.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import './App.css';
+import Clock from './Clock';
+import { useState, useEffect } from 'react';
 
-function Recipe({ drinkers }) {
-  return (
-    <ol>    
-      <li>Boil {drinkers} cups of water.</li>
-      <li>Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice.</li>
-      <li>Add {0.5 * drinkers} cups of milk to boil and sugar to taste.</li>
-    </ol>
-  );
+function useTime() {
+  const [time, setTime] = useState(() => new Date());
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(id);
+  }, []);
+  return time;
 }
 
 export default function App() {
+  const time = useTime();
   return (
-    <section>
-      <h1>Spiced Chai Recipe</h1>
-      <h2>For two</h2>
-      <Recipe drinkers={2} />
-      <h2>For a gathering</h2>
-      <Recipe drinkers={4} />
-    </section>
+    <Clock time={time} />
   );
 }
--- src/Clock.js
+++ src/Clock.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-export default function Clock({ color, time }) {
-    return (
-      <h1 style={{ color: color }}>
-        {time}
-      </h1>
-    );
+export default function Clock({ time }) {
+  let hours = time.getHours();
+  let className;
+  if (hours >= 0 && hours <= 6) {
+    className = 'night';
+  } else {
+    className = 'day';
   }
+  return (
+    <h1 className={className}>
+      {time.toLocaleTimeString()}
+    </h1>
+  );
+}
