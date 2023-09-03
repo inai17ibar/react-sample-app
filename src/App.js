908198ec860df987a91e4e0a1dca9bc77653a5aa
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-03 17:41:54 +0900

    Delete global variance and Add props with person

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
-import ClockApp from './ClockApp';
+import ProfileCard from './ProfileCard';
 
 export default function App() {
   return (
-    <ClockApp></ClockApp>
+    <>
+      <ProfileCard person={{
+        imageId: 'lrWQx8l',
+        name: 'Subrahmanyan Chandrasekhar',
+      }} />
+      <ProfileCard person={{
+        imageId: 'MK3eW3A',
+        name: 'Creola Katherine Johnson',
+      }} />
+    </>
   )
 }
---
+++ src/ProfileCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Panel from './Panel.js';
+import { getImageUrl } from './utils.js';
+
+export default function ProfileCard({ person }) {
+  return (
+    <Panel>
+      <Header person={person}/>
+      <Avatar person={person}/>
+    </Panel>
+  )
+}
+
+function Header({person}) {
+  return <h1>{person.name}</h1>;
+}
+
+function Avatar({person}) {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={50}
+      height={50}
+    />
+  );
+}
