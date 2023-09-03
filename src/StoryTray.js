45c04d377b770d74fee85f3c173d9d5121ee4531
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-03 17:57:02 +0900

    Fix a bug of infinity loop

---
+++ src/StoryTray.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function StoryTray({ stories }) {
+    return (
+      <ul>
+        {stories.map(story => (
+          <li key={story.id}>
+            {story.label}
+          </li>
+        ))}
+        <li>Create Story</li>
+      </ul>
+    );
+  }
