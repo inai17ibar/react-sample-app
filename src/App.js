6c4742fc3f96cd038f7dce6100886e5768961a40
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-08 23:43:18 +0900

    try to practice rendering update variable

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React from 'react';
 import './App.css';
 import { useState } from 'react';
 
-export default function Counter() {
-  const [number, setNumber] = useState(0);
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true);
+
+  function handleClick() {
+    setWalk(!walk);
+    alert(walk ? 'Stop is next' : 'Walk is next');
+  }
 
   return (
     <>
-      <h1>{number}</h1>
-      <button onClick={() => {
-        setNumber(number + 5);
-        setTimeout(() => {
-          alert(number);
-        }, 3000);
-      }}>+5</button>
+      <button onClick={handleClick}>
+        Change to {walk ? 'Stop' : 'Walk'}
+      </button>
+      <h1 style={{
+        color: walk ? 'darkgreen' : 'darkred'
+      }}>
+        {walk ? 'Walk' : 'Stop'}
+      </h1>
     </>
-  )
+  );
 }
+
