6542c8f67296b7584623c8bcff11005b75a6ce87
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-08 23:24:22 +0900

    Set state triggers renders

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import './App.css';
-import { useState, useEffect } from 'react';
-import Clock2 from './Clock2.js';
+import { useState } from 'react';
 
-function useTime() {
-  const [time, setTime] = useState(() => new Date());
-  useEffect(() => {
-    const id = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-    return () => clearInterval(id);
-  }, []);
-  return time;
+export default function Form() {
+  const [isSent, setIsSent] = useState(false);
+  const [message, setMessage] = useState('Hi!');
+  if (isSent) {
+    return <h1>Your message is on its way!</h1>
+  }
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      setIsSent(true);
+      sendMessage(message);
+    }}>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
 }
 
-export default function App() {
-  const time = useTime();
-  return (
-    <Clock2 time={time.toLocaleTimeString()} />
-  );
+function sendMessage(message) {
+  // ...
 }
