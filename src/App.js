8e0cee2d3f4f8c16edeb49903954d536d946c68c
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-12 00:02:18 +0900

    Find and fix the mutation

--- src/App.js
+++ src/App.js
@@ -1,59 +1,57 @@
 import React from 'react';
 import './App.css';
 import { useState } from 'react';
-import { useImmer } from 'use-immer';
+import Background from './Background.js';
+import Box from './Box.js';
 
-export default function Scoreboard() {
-  const [player, setPlayer] = useState({
-    firstName: 'Ranjani',
-    lastName: 'Shettar',
-    score: 10,
+export default function Canvas() {
+  const initialPosition = {
+    x: 0,
+    y: 0
+  };
+
+  const [shape, setShape] = useState({
+    color: 'orange',
+    position: initialPosition
   });
 
-  function handlePlusClick() {
-    setPlayer({
-      ...player,
-      score: player.score + 1, //数値はこう書く
+  function handleMove(dx, dy) {
+    setShape({
+      ...shape,
+      position: {
+        x: shape.position.x + dx,
+        y: shape.position.y + dy,
+      }
     });
   }
 
-  function handleFirstNameChange(e) {
-    setPlayer({
-      ...player,
-      firstName: e.target.value,
-    });
-  }
-
-  function handleLastNameChange(e) {
-    setPlayer({
-      ...player,
-      lastName: e.target.value
+  function handleColorChange(e) {
+    setShape({
+      ...shape,
+      color: e.target.value
     });
   }
 
   return (
     <>
-      <label>
-        Score: <b>{player.score}</b>
-        {' '}
-        <button onClick={handlePlusClick}>
-          +1
-        </button>
-      </label>
-      <label>
-        First name:
-        <input
-          value={player.firstName}
-          onChange={handleFirstNameChange}
-        />
-      </label>
-      <label>
-        Last name:
-        <input
-          value={player.lastName}
-          onChange={handleLastNameChange}
-        />
-      </label>
+      <select
+        value={shape.color}
+        onChange={handleColorChange}
+      >
+        <option value="orange">orange</option>
+        <option value="lightpink">lightpink</option>
+        <option value="aliceblue">aliceblue</option>
+      </select>
+      <Background
+        position={initialPosition}
+      />
+      <Box
+        color={shape.color}
+        position={shape.position}
+        onMove={handleMove}
+      >
+        Drag me!
+      </Box>
     </>
   );
 }
