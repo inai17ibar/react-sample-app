c104ff485cdd5f97f3c639c98684c3c1786eba49
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-11 23:51:39 +0900

    Fix incorrect state updates

--- src/App.js
+++ src/App.js
@@ -3,81 +3,57 @@ import './App.css';
 import { useState } from 'react';
 import { useImmer } from 'use-immer';
 
-export default function Form() {
-  const [person, updatePerson] = useImmer({
-    name: 'Niki de Saint Phalle',
-    artwork: {
-      title: 'Blue Nana',
-      city: 'Hamburg',
-      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
-    }
+export default function Scoreboard() {
+  const [player, setPlayer] = useState({
+    firstName: 'Ranjani',
+    lastName: 'Shettar',
+    score: 10,
   });
 
-  function handleNameChange(e) {
-    updatePerson(draft => {
-      draft.name = e.target.value;
+  function handlePlusClick() {
+    setPlayer({
+      ...player,
+      score: player.score + 1, //数値はこう書く
     });
   }
 
-  function handleTitleChange(e) {
-    updatePerson(draft => {
-      draft.artwork.title = e.target.value;
+  function handleFirstNameChange(e) {
+    setPlayer({
+      ...player,
+      firstName: e.target.value,
     });
   }
 
-  function handleCityChange(e) {
-    updatePerson(draft => {
-      draft.artwork.city = e.target.value;
-    });
-  }
-
-  function handleImageChange(e) {
-    updatePerson(draft => {
-      draft.artwork.image = e.target.value;
+  function handleLastNameChange(e) {
+    setPlayer({
+      ...player,
+      lastName: e.target.value
     });
   }
 
   return (
     <>
       <label>
-        Name:
+        Score: <b>{player.score}</b>
+        {' '}
+        <button onClick={handlePlusClick}>
+          +1
+        </button>
+      </label>
+      <label>
+        First name:
         <input
-          value={person.name}
-          onChange={handleNameChange}
+          value={player.firstName}
+          onChange={handleFirstNameChange}
         />
       </label>
       <label>
-        Title:
+        Last name:
         <input
-          value={person.artwork.title}
-          onChange={handleTitleChange}
+          value={player.lastName}
+          onChange={handleLastNameChange}
         />
       </label>
-      <label>
-        City:
-        <input
-          value={person.artwork.city}
-          onChange={handleCityChange}
-        />
-      </label>
-      <label>
-        Image:
-        <input
-          value={person.artwork.image}
-          onChange={handleImageChange}
-        />
-      </label>
-      <p>
-        <i>{person.artwork.title}</i>
-        {' by '}
-        {person.name}
-        <br />
-        (located in {person.artwork.city})
-      </p>
-      <img 
-        src={person.artwork.image} 
-        alt={person.artwork.title}
-      />
     </>
   );
 }
