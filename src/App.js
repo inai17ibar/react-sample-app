0966522db3d49a224739cccc6826c8832e20678e
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-12 00:44:24 +0900

    Removing from an array

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { useState } from 'react';
 
-let nextId = 0;
+let initialArtists = [
+  { id: 0, name: 'Marta Colvin Andrade' },
+  { id: 1, name: 'Lamidi Olonade Fakeye'},
+  { id: 2, name: 'Louise Nevelson'},
+];
 
 export default function List() {
-  const [name, setName] = useState('');
-  const [artists, setArtists] = useState([]);
+  const [artists, setArtists] = useState(
+    initialArtists
+  );
 
   return (
     <>
       <h1>Inspiring sculptors:</h1>
-      <input
-        value={name}
-        onChange={e => setName(e.target.value)}
-      />
-      <button onClick={() => {
-        setArtists([
-          ...artists,
-          { id: nextId++, name: name }
-        ]);
-      }}>Add</button>
       <ul>
-        {artists.map(artist => (
-          <li key={artist.id}>{artist.name}</li>
+        {artists.map(artist => ( //選択肢のレンダリングもmapでやっている
+          <li key={artist.id}>
+            {artist.name}{' '}
+            <button onClick={() => {
+              setArtists(
+                artists.filter(a =>
+                  a.id !== artist.id //一致していないものだけを残すフィルター
+                )
+              );
+            }}>
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
