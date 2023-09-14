b413962ade7ede9d0a156de7c7e1032efecbdfdb
Soichiro Inatani <inai17ibar@gmail.com>
2023-09-15 00:03:05 +0900

    Preserving and resetting state using key in chat

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
 import React from 'react';
 import { useState } from 'react';
+import Chat from './Chat.js';
+import ContactList from './ContactList.js';
 
-export default function Accordion() {
-  //親が子の状態をもつ方法
-  const [activeIndex, setActiveIndex] = useState(0);
+export default function Messenger() {
+  const [to, setTo] = useState(contacts[0]);
   return (
-    <>
-      <h2>Almaty, Kazakhstan</h2>
-      <Panel
-        title="About"
-        isActive={activeIndex === 0}
-        onShow={() => setActiveIndex(0)}
-      >
-        With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
-      </Panel>
-      <Panel
-        title="Etymology"
-        isActive={activeIndex === 1}
-        onShow={() => setActiveIndex(1)}
-      >
-        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered a likely candidate for the ancestor of the modern domestic apple.
-      </Panel>
-    </>
-  );
+    <div>
+      <ContactList
+        contacts={contacts}
+        selectedContact={to}
+        onSelect={contact => setTo(contact)}
+      />
+      <Chat key={to.email} contact={to} /> 
+    </div>
+  )
 }
 
-function Panel({
-  title,
-  children,
-  isActive,
-  onShow
-}) {
-  return (
-    <section className="panel">
-      <h3>{title}</h3>
-      {isActive ? (
-        <p>{children}</p> //childrenがpanelの子を表す？
-      ) : (
-        <button onClick={onShow}>
-          Show
-        </button>
-      )}
-    </section>
-  );
-}
+const contacts = [
+  { name: 'Taylor', email: 'taylor@mail.com' },
+  { name: 'Alice', email: 'alice@mail.com' },
+  { name: 'Bob', email: 'bob@mail.com' }
+];
---
+++ src/ContactList.js
@@ -0,0 +1,23 @@
+import React from "react";
+export default function ContactList({
+    selectedContact,
+    contacts,
+    onSelect
+  }) {
+    return (
+      <section className="contact-list">
+        <ul>
+          {contacts.map(contact =>
+            <li key={contact.email}>
+              <button onClick={() => {
+                onSelect(contact);
+              }}>
+                {contact.name}
+              </button>
+            </li>
+          )}
+        </ul>
+      </section>
+    );
+  }
+  
